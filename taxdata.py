32d16bf90786fb228346d0ee996db41f43bc8b29
sebastiansajie <sebastiansajie@yahoo.com>
2018-11-13 05:08:07 -0500

    rename the samplefile

--- taxdata.py
+++ taxdata.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-data = pd.read_csv('SAMPLE_ITR/SAMPLE_AY_2017-18.TXT', sep='|')
+data = pd.read_csv('SAMPLE_ITR/ITR3-SAMPLE_AY_2017-18.TXT', sep='|')
 data.to_csv('pit.csv', index=False)
