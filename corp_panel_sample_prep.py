1599976c04a34573d25cda993cb84b027c74b4e0
Cody Kallen <cody_kallen@yahoo.com>
2019-01-16 12:06:52 +0530

    New cross-section

--- corp_panel_sample_prep.py
+++ corp_panel_sample_prep.py
@@ -14,29 +14,57 @@ as totals for other measures and the distribution of firm sizes.
 import pandas as pd
 import numpy as np
 
-# Get ful lpanel data
+# Get full panel data
 data_full = pd.read_excel('ITR6_2013_2015_PANEL_MERGE_SHORT_SAMPLE1.xlsx',
                           sheet_name='ITR6_2013_2015_PANEL_MERGE_SHOR')
+
+# Rename some variables
+renames = {'SHORT_TERM_15PER': 'ST_CG_AMT_1', 'SHORT_TERM_30PER': 'ST_CG_AMT_2',
+           'LONG_TERM_10PER': 'LT_CG_AMT_1', 'LONG_TERM_20PER': 'LT_CG_AMT_2',
+           'SHORT_TERM_APPRATE': 'ST_CG_AMT_APPRATE',
+           'TOTAL_INCOME_ALL':'GTI_BEFORE_LOSSES'}
+data_full = data_full.rename(renames, axis=1)
+data_full = data_full.fillna(0)
+
 # Get results for 2013
 data13 = data_full[data_full['ASSESSMENT_YEAR'] == 2013].reset_index()
 count = len(data13)
-# Aggregate totals for various measures
-agg_results = {'no_returns': 1}
+
+# Variable list we need to use
+varlist = ['INCOME_HP', 'PRFT_GAIN_BP_OTHR_SPECLTV_BUS',
+           'PRFT_GAIN_BP_SPECLTV_BUS', 'PRFT_GAIN_BP_SPCFD_BUS',
+           'PRFT_GAIN_BP_INC_115BBF', 'ST_CG_AMT_1', 'ST_CG_AMT_2',
+           'LT_CG_AMT_1', 'LT_CG_AMT_2']
+# Average amounts for various measures
+agg_results = {'no_returns': 781141.,
+               'INCOME_HP': 134403176952 / 790443.,
+               'PRFT_GAIN_BP_OTHR_SPECLTV_BUS': 11650386829465 / 783662.,
+               'PRFT_GAIN_BP_SPECLTV_BUS': 2850073821 / 783662.,
+               'PRFT_GAIN_BP_SPCFD_BUS': 27604158172 / 783662.,
+               'PRFT_GAIN_BP_INC_115BBF': 147539582 / 783662.,
+               'ST_CG_AMT_1': 82338302877 / 781141.,
+               'ST_CG_AMT_2': 4641554226 / 781141.,
+               'LT_CG_AMT_1': 250513034751 / 781141.,
+               'LT_CG_AMT_2': 485197199930 / 781141.}
 
 # Totals in the sample
 sample_results = {'no_returns': count}
+blowup_results = {}
+
+for var in varlist:
+    sample_results[var] = 1.0 * sum(data13[var]) / count
+    blowup_results[var] = agg_results[var] / sample_results[var]
 
 # Produce weights for each observation in 2013
 WT2013 = np.array([agg_results['no_returns'] / sample_results['no_returns']] *
                   count)
 
-def calc_blowup(measure):
-    """
-    Calculates the blowup factor for measure. Measure must be a string matching
-    a variable name in sample_results and an aggregate amount in agg_results.
-    """
-    total13 = sum(data13[measure] * WT2013)
-    total17 = agg_results[measure]
-    blowup = total17 / total13
-    return blowup
+# Export CSV and blowup results
+blowup_df = pd.DataFrame(blowup_results)
+blowup_df.round(6)
+blowup_df.to_csv('cit_panel_blowup.csv', index=False)
+data_full.round(6)
+data_full.to_csv('cit_panel.csv', index=False)
+
+
 
